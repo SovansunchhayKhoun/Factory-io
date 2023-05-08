9171f0b960fc6b415a6f717bffc8036a9b2490cf
Charmzz <118677880+Tepinsim@users.noreply.github.com>
2023-05-08 18:50:41 +0700

    update

--- react-vite/src/views/Communitylanding.jsx
+++ react-vite/src/views/Communitylanding.jsx
@@ -2,7 +2,16 @@ export const Communitylanding = () => {
     return (
         <>
         <main>
-          
+          <div className="justify-center">
+            <h1>Our Community</h1>
+          </div>
+          <div className="px-48 flex justify-center">
+            <div className="mr-12">
+              <span className="font-semibold text-2xl leading-10">
+                Upload and share your <br></br> Project with our community
+              </span>
+            </div>
+          </div>
         </main>
         </>
     );
