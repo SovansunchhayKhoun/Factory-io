93201e0382f8c0c7abd638966614ac6ad9436f43
sunchhay395 <sunchhay395@gmail.com>
2023-05-30 10:17:47 +0700

    made a few more components responsive

--- react-vite/src/views/Makerio/ItemView.jsx
+++ react-vite/src/views/Makerio/ItemView.jsx
@@ -9,18 +9,25 @@ import "react-multi-carousel/lib/styles.css";
 import {ItemCardCarousel} from "../../components/CartComponents/ItemCardCarousel.jsx";
 import {ProductReview} from "../../components/ProductReview.jsx";
 
-
-
 export const ItemView = (props) => {
   let {id} = useParams();
-  const {item, getItem, items, itemsQueryReFetch,reviewsQueryReFetch} = useContext(ProductContext);
+  const {item, getItem, items, itemsQueryReFetch, reviewsQueryReFetch} = useContext(ProductContext);
   const {scrollTop} = useContext(InvoiceContext);
   useEffect(() => {
     getItem(id);
     itemsQueryReFetch();
-    reviewsQueryReFetch();
+    // reviewsQueryReFetch();
     scrollTop(0);
   }, []);
+
+  const getType = () => {
+    switch (item.type) {
+      case 'sensor':
+        return 'Sensor';
+      case 'MicroController':
+        return 'Micro-Controller';
+    }
+  }
 
   const responsive = {
     superLargeDesktop: {
@@ -41,6 +48,7 @@ export const ItemView = (props) => {
       items: 2
     }
   };
+
   const {addToCart, cartItem} = useContext(CartContext);
   const {token} = useAuthContext();
   const itemCart = cartItem.find((i) => i.id === item.id);
@@ -56,26 +64,39 @@ export const ItemView = (props) => {
         </button>
         <span className="font-bold text-xl">Product</span>
       </div>
-      <section className="mt-12 flex justify-center gap-x-12">
-        <div className="flex items-center border-2 border-tealBase p-4">
+      <section className="xl:px-48 mt-12 lg:flex lg:flex-row lg:justify-center lg:gap-x-12
+        flex flex-col gap-6">
+        {/*product image*/}
+        <div className="xl:flex-grow-0 flex-1 flex items-center justify-center border-2 border-tealBase p-4">
           {(item?.image === null || item?.image === undefined)
-            ? <img className="max-w-[350px] max-h-[350px] min-w-[350px] min-h-[350px] object-contain"
+            ? <img className="xl:min-w-[500px] xl:min-h-[500px]
+              lg:max-w-[300px]
+              object-contain"
                    src="/assets/images/makerio.png" alt={item.name}/>
-            : <img className="max-w-[350px] max-h-[350px] min-w-[350px] min-h-[350px] object-contain"
-                   src={`http://127.0.0.1:8000/${item.image}`} alt={item.name}/>
+            : <img
+              className="xl:min-w-[500px] xl:min-h-[500px]
+              lg:max-w-[300px]
+              object-contain"
+              src={`http://127.0.0.1:8000/${item.image}`} alt={item.name}/>
           }
         </div>
-        <div className="text-lg shadow-2xl rounded-xl p-4">
-          <div className="mb-2 font-bold text-blueBase">${item.price}</div>
-          <div className="mb-2 font-bold text-tealBase">{item.name}</div>
-          <div className="mb-2 text-blackFactory">Item Type: <span className={`font-semibold`}> {item.type}</span></div>
-          <div className="mb-2 text-redBase font-bold">{currentQty === 0 ? "Out of Stock" : "In stock"}</div>
-          <div className="mb-2 text-blackFactory">
-            {/*Description*/}
-            <p dangerouslySetInnerHTML={{__html: item.description}} className="mb-3"/>
+        {/*product description*/}
+        <div
+          className="xl:flex-grow-0 flex-1 flex flex-col xl:min-w-[500px] xl:min-h-[500px]
+          lg:min-w-[300px] lg:text-lg shadow-2xl rounded-[20px] border border-black p-4 ">
+          <div className="lg:text-lg md:text-base text-sm mb-2 font-bold text-blueBase">${item.price}</div>
+          <div className="lg:text-lg md:text-base text-sm mb-2 font-bold text-tealBase">{item.name}</div>
+          <div className="lg:text-lg md:text-base text-sm mb-2 text-blackFactory">Item Type: <span
+            className={`font-semibold`}>{getType()}</span></div>
+          <div
+            className="lg:text-lg md:text-base text-sm mb-2 text-redBase font-bold">{currentQty === 0 ? "Out of Stock" : "In stock"}</div>
 
+          {/*Description*/}
+          <div className="sm:max-w-[80%] w-full">
+            <p dangerouslySetInnerHTML={{__html: item.description}}
+               className="lg:text-lg md:text-base text-sm mb-3 text-blackFactory"/>
           </div>
-          <div className="flex justify-end items-center">
+          <div className="mt-auto flex justify-end items-center">
             <button
               className={`${item.status === 0 && 'hidden'} rounded-[50%] px-1 py-1 hover:bg-tealActive active:bg-tealBase transition duration-300`}
               onClick={() => {
@@ -83,25 +104,25 @@ export const ItemView = (props) => {
                   addToCart(item, currentQty);
                 }
               }}>
-              <img src="/assets/images/cart-icon.png" alt=""/>
+              <img className="md:w-12 w-10" src="/assets/images/cart-icon.png" alt=""/>
             </button>
           </div>
         </div>
       </section>
-      <ProductReview item={item}/>
-      <div className="mt-16 mb-16">
-        <div className="mb-3 text-tealHover font-semibold">Related items</div>
-        <Carousel itemClass={'flex'} responsive={responsive}>
-          {
-            items.filter(itemFilter => itemFilter.type === item.type && itemFilter.id !== item.id)
-              .map((item, key) => {
-                return (
-                  <ItemCardCarousel item={item} key={key}/>
-                )
-              })}
 
-        </Carousel>
-      </div>
+      {/*<ProductReview item={item}/>*/}
+      {/*<div className="mt-16 mb-16">*/}
+      {/*  <div className="mb-3 text-tealHover font-semibold">Related items</div>*/}
+      {/*  <Carousel itemClass={'flex'} responsive={responsive}>*/}
+      {/*    {*/}
+      {/*      items.filter(itemFilter => itemFilter.type === item.type && itemFilter.id !== item.id)*/}
+      {/*        .map((item, key) => {*/}
+      {/*          return (*/}
+      {/*            <ItemCardCarousel item={item} key={key}/>*/}
+      {/*          )*/}
+      {/*        })}*/}
+      {/*  </Carousel>*/}
+      {/*</div>*/}
     </>
   );
 };
