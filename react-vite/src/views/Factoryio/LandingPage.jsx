9162c08ded0c3d1eb18c084b6b5e1eec7190fdcf
sunchhay395 <sunchhay395@gmail.com>
2023-06-10 17:26:16 +0700

    Update Factory layout

--- react-vite/src/views/Factoryio/LandingPage.jsx
+++ react-vite/src/views/Factoryio/LandingPage.jsx
@@ -3,12 +3,13 @@ import {UserCard} from "../../components/UserCard.jsx";
 
 export const LandingPage = () => {
   return (
-    <main className="">
-      <div className="flex justify-center mb-12 columns-2">
-        <div className="mr-12 ">
-        <span className="font-bold text-2xl leading-10">
-          RESEARCH COMMUNITY <br/> FOR DEVELOPMENT
-        </span>
+    <main className="flex flex-col gap-12">
+
+      <section className="flex justify-between">
+        <div className="">
+          <div className="font-bold text-2xl leading-10">
+            RESEARCH COMMUNITY <br/> FOR DEVELOPMENT
+          </div>
           <div className="leading-7 mb-3">
             MAKE YOUR IMAGINATION <br/>
             BECOME REALITY WITH <br/>
@@ -18,47 +19,49 @@ export const LandingPage = () => {
             Join Us
           </button>
         </div>
-        <div className="w-[448px] h-[333px] mr-10 justify-end">
+        <div className="w-[448px] h-[333px] justify-end">
           <img src="/assets/images/board.jpg" alt=""/>
         </div>
-      </div>
-      <div className="flex flex-col justify-center">
+      </section>
+
+      <section className="flex flex-col justify-center">
         <span className="font-bold leading-4">Featured on</span>
         <div className="flex flex-row columns-4 items-center mt-10 h-20 justify-between ">
-          <img className="flex h-[20px]" src="/assets/images/factory.png" alt="" />
-          <img className="flex h-[25px]" src="/assets/images/makerio.png" alt="" />
-          <img className="flex h-[30px]" src="/assets/images/robotx.png" alt="" />
-          <img className="flex h-[60px]" src="/assets/images/reachsa.png" alt="" />
+          <img className="flex h-[20px]" src="/assets/images/factory.png" alt=""/>
+          <img className="flex h-[25px]" src="/assets/images/makerio.png" alt=""/>
+          <img className="flex h-[30px]" src="/assets/images/robotx.png" alt=""/>
+          <img className="flex h-[60px]" src="/assets/images/reachsa.png" alt=""/>
         </div>
-      </div>
+      </section>
 
-      <div className="px-40 mb-10 flex justify-center">
-        <div className="flex items-center w-1/2 h-[300px] mr-10">
-          <img src="/assets/images/Sound-Detection-Sensor-Module.jpg" alt=""/>
+      <section className="flex justify-between items-start">
+        <div className="flex items-center">
+          <img width={448} src="/assets/images/Sound-Detection-Sensor-Module.jpg" alt=""/>
         </div>
-          <div className="flex flex-col justify-center w-1/2">
-            <span className="font-semibold text-2xl leading-10">
-              Shop Your Component With Maker.io
-            </span>
-            <div>
-              <div className="leading-7 mb-3">
-                Start your project now and shop
-                your component with us
-              </div>
-              <button className="bg-white border border-redHover text-red rounded-[50px] px-3 py-1">
-                Buy Now
-              </button>
+        <div className="flex flex-col justify-center">
+          <span className="font-semibold text-2xl leading-10">
+            Shop Your Component <br/> With Maker.io
+          </span>
+          <div>
+            <div className="leading-7">
+              Start your project now and shop <br/>
+              your component with us
             </div>
+            <button className="bg-white border border-redHover text-red rounded-[50px] px-3 py-1">
+              Buy Now
+            </button>
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="px-40 flex justify-center">
-        <div className="flex flex-col justify-start w-1/2 mr-4">
+      <section className="flex justify-between">
+
+        <div className="flex flex-col justify-start">
           <div className="flex font-semibold text-2xl leading-10">
             Upload Your Project With Our Community
           </div>
           <div>
-            <div className="flex leading-7 mb-2">
+            <div className="flex leading-7">
               Sharing knowledge
               and your achievement with us
             </div>
@@ -68,24 +71,26 @@ export const LandingPage = () => {
           </div>
         </div>
 
-        <div className="mr-10 w-1/2 h-[300px] ">
-          <video autoPlay controls loop muted>
+        <div className="">
+          <video width={448} autoPlay controls loop muted>
             <source src="/assets/images/vdpro.mp4" type="video/mp4"></source>
             Your browser does not support the video tag.
           </video>
         </div>
-      </div>
 
-      <div className="px-40 flex justify-center h-[300px]">
-        <div className="flex justify-start w-[450px] h-[250px] mr-4">
-          <img src="/assets/images/mrc.jpg" alt=""/>
+      </section>
+
+      <section className="flex justify-between">
+
+        <div className="flex justify-start mr-4">
+          <img width={448} src="/assets/images/mrc.jpg" alt=""/>
         </div>
-        <div className="flex flex-col w-1/2">
+        <div className="flex flex-col">
           <span className="font-semibold text-2xl leading-10">
             Check Out Recent Technology Contest
           </span>
           <div>
-            <div className="leading-7 mb-2">
+            <div className="leading-7">
               New Contest will be updated and
               we courage everyone in our community to join.
             </div>
@@ -94,34 +99,41 @@ export const LandingPage = () => {
             </button>
           </div>
         </div>
-      </div>
-      <div className="px-38 mb-12 flex flex-col items-center p-2 font-medium">
+      </section>
+
+      <section className="flex flex-col items-center gap-6 font-medium">
+
         <div className="font-semibold text-2xl">
           About Us
         </div>
-        <div className="flex flex-row justify-between mt-4">
-          <div className="flex justify-start leading-7 mr-12 font-normal w-1/2 ">
+
+        <div className="flex justify-between">
+          <div className="flex-1 leading-7 font-normal">
             Factory.io is working on the production and community of Maker, improvement and distribution of technology
             products. Manufacturers incorporate new technologies including Internet of Things (IoT), cloud computing and
             analytics, and AI and machine learning and Robotic and PLC systems and STEAM for Kid into their production
             and automation facilities.
           </div>
-          <div className="flex justify-end mt-4 w-1/2">
-            <img src="/assets/images/abtus.jpg" alt=""/>
+          <div className="flex-1 flex justify-end">
+            <img width={448} src="/assets/images/abtus.jpg" alt=""/>
           </div>
         </div>
-      </div>
-      <div className="px-48 flex flex-col items-center gap-y-3 mt-5">
-        <div className="flex flex-col items-center gap-y-6 p-2 font-semibold text-2xl">
+
+      </section>
+
+      <section className="flex flex-col items-center gap-6">
+        <div className="font-semibold text-2xl">
           Our Community
         </div>
+
         <div>
-            <span className="flex flex-col justify-center-center gap-y-6 p-2 font-normal text-center mb-12">
-              The IoT (Internet of Things) platform community consists of a broad spectrum of people, groups, and businesses engaged in the creation, implementation, and usage of IoT platforms. For the purpose of facilitating the development of scalable and interoperable IoT systems, its focus is on defining standards, best practices, and working together on open-source projects. The community, which consists of software providers, system integrators, hardware producers, network operators, and end users, collaborates to address the technical, commercial, and legal issues related to IoT deployment and to create new use cases and applications that make use of IoT platforms.
-            </span>
+          <span className="flex flex-col justify-center-center text-center">
+            The IoT (Internet of Things) platform community consists of a broad spectrum of people, groups, and businesses engaged in the creation, implementation, and usage of IoT platforms. For the purpose of facilitating the development of scalable and interoperable IoT systems, its focus is on defining standards, best practices, and working together on open-source projects. The community, which consists of software providers, system integrators, hardware producers, network operators, and end users, collaborates to address the technical, commercial, and legal issues related to IoT deployment and to create new use cases and applications that make use of IoT platforms.
+          </span>
         </div>
-        <div className="flex flex-col items-center gap-16 ">
-          <div className="grid grid-cols-5 md:flex" >
+
+        <div className="flex flex-col items-center gap-16 mt-12">
+          <div className="grid grid-cols-5 auto-cols-fr">
             <UserCard pf="seangly.jpg" name="Sou Seangly" position="Factory.io" management="Project superviser"
                       fbL="https://web.facebook.com/profile.php?id=100022815573502"
                       LL="https://www.linkedin.com/in/seangly-sou-20a9a81ab/"/>
@@ -137,17 +149,19 @@ export const LandingPage = () => {
                       fbL="https://web.facebook.com/channudam.ray.9"
                       LL="https://www.linkedin.com/in/chann-udam-ray-aa3387200"/>
           </div>
-          <div className="grid grid-cols-3 ">
+          <div className="grid grid-cols-3 auto-cols-fr">
             <UserCard pf="kakada.jpg" name="Noch Kakada" position="Reachsa.io" management="PCB Coordinator"
-                      fbL="https://web.facebook.com/profile.php?id=100032315051455" LL="https://www.linkedin.com/in/noch-kakada-953bb7274"/>
+                      fbL="https://web.facebook.com/profile.php?id=100032315051455"
+                      LL="https://www.linkedin.com/in/noch-kakada-953bb7274"/>
             <UserCard pf="ratanak.jpg" name="Brorn Munyratanak" position="Maker.io"
                       management="Production Control Coordinator" fbl="https://web.facebook.com/gkmuny.ratanak.1"
                       LL="https://www.linkedin.com/in/brorn-munyratanak-9ab526254"/>
-            <UserCard pf="marot.jpg" name="Tep Marot" position="Maker.io" management="Project Led" fbL="https://www.facebook.com/daniel.ohhhyeaaa?mibextid=ZbWKwL" LL="https://www.linkedin.com/in/tep-marot-575973262"/>
+            <UserCard pf="marot.jpg" name="Tep Marot" position="Maker.io" management="Project Led"
+                      fbL="https://www.facebook.com/daniel.ohhhyeaaa?mibextid=ZbWKwL"
+                      LL="https://www.linkedin.com/in/tep-marot-575973262"/>
           </div>
         </div>
-      </div>
-
+      </section>
     </main>
   );
 };
