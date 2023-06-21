7bdab139360cadedffb6169377c66b0263102f70
Charmzz <118677880+Tepinsim@users.noreply.github.com>
2023-06-21 11:46:31 +0700

    fix palce order

--- react-vite/src/views/Makerio/CartView.jsx
+++ react-vite/src/views/Makerio/CartView.jsx
@@ -15,7 +15,7 @@ export const CartView = () => {
   const {user, isLoading} = useAuthContext();
   const {userAddress, getUserAddress, addressLoading} = useAddressContext();
   const {cartItem, getCartItem} = useContext(CartContext);
-  const {setAddress, address, latitude, longitude, getAddress} = useContext(GoogleMapsContext);
+  const {setAddress, address, latitude, longitude, placeId, setPlaceId} = useContext(GoogleMapsContext);
 
   useEffect(() => {
     const getAddress = async (lat, lng) => {
@@ -24,6 +24,8 @@ export const CartView = () => {
         .then(res => {
           if (res.results[0]) {
             setAddress(res.results[0].formatted_address);
+            setPlaceId(res.results[0].place_id)
+            // console.log(placeId)
           }
         }).catch(e => console.log(e))
     }
