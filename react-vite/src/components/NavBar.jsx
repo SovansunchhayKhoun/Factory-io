9c5551e338e472567111e7140b1196d1fd613c7c
RethtihpongEM <rithtipongem@gmail.com>
2023-06-23 21:03:28 +0700

    nav bar for phone view

--- react-vite/src/components/NavBar.jsx
+++ react-vite/src/components/NavBar.jsx
@@ -241,9 +241,28 @@ export const NavBar = (props) => {
               <input type="text"
                      placeholder="Search..."
                      className="w-full rounded-md focus:ring-0 focus:border-tealBase border-2 border-tealBase"
+                     value={searchInput}
                      onChange={event => {
-                       setSearchInput(event.target.value)
+                       handleSearchInput(event)
                      }}/>
+              <div
+                className={`flex flex-col gap-4 z-10 border border-gray-200 rounded-md absolute bg-white top-[75px] lg:w-[384px] cursor-pointer ${searchInput === "" && 'hidden'}`}>
+                {filteredItem?.length === 0 && <div className="mx-auto mt-2"> No item found</div>}
+                {filteredItem?.slice(0, 5).map((item, key) => {
+                  return (
+                    <Link to={`${item.id}`} onClick={e => {
+                      e.stopPropagation()
+                      getItem(item.id)
+                      setSearchInput("")
+                    }} key={key} className="px-4 py-2 flex flex-row justify-between items-center hover:bg-gray-100">
+                      <p>
+                        {item.name}
+                      </p>
+                      <img className="w-[50px]" src={`http://127.0.0.1:8000/${item.image}`} alt={item.name}/>
+                    </Link>
+                  )
+                })}
+              </div>
             </AccordionBody>
           </Accordion>
         </Fragment>
